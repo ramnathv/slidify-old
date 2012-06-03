9fc4e8e6f6d0a4245edd1ab986cc4cff5f54f103
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2012-06-03 00:33:58 -0400

    move slide2list to utils.R

--- R/utils.R
+++ R/utils.R
@@ -35,3 +35,40 @@ add_theme <- function(theme){
   file.copy(css_file, theme_file)
   cat(whisker.render(template), file = theme_file, append = TRUE)
 }
+
+slide2list <- function(slide){
+	require(markdown); 
+	content  <- markdownToHTML(text = slide, options = c('fragment_only'))
+	content  <- readLines(textConnection(content))
+	article  <- NULL
+	fx_regex <- '^<p>(..article:\\s?)(.*?)</p>\n?$'
+	fx_match <- grep(fx_regex, content)
+	if (length(fx_match) == 1) {
+		article  <- gsub(fx_regex, '\\2', content[fx_match])
+		content  <- content[-fx_match]
+	}
+	
+	# CHECK FOR INCREMENTAL LISTS 
+	ul_regex <- '^<p>(..ul:\\s?)(.*?)</p>\n?$'
+	ul_match <- grep(ul_regex, content)
+	if (length(ul_match) >= 1) {
+		ul_loc   <- grep('^<ul>', content)
+		ul_class <- gsub(ul_regex, "\\2", content[ul_match])
+		content[ul_loc]  <- sprintf('<ul class= "%s">', ul_class)
+		content <- content[-ul_match]
+	}
+	
+	# CHECK FOR INCREMENTAL LISTS IN  DECK.JS
+	li_regex <- '^<p>(..li:\\s?)(.*?)</p>\n?$'
+	li_match <- grep(li_regex, content)
+	if (length(li_match) >= 1) {
+		li_loc   <- grep('^<li>', content)
+		li_class <- gsub(li_regex, "\\2", content[li_match])
+		content[li_loc]  <- gsub("^<li>", sprintf('<li class= "%s">', li_class), content[li_loc])
+		content <- content[-li_match]
+	}
+	
+	content  <- paste(content, collapse = '\n')
+	id <- make_id(length = 4)
+	list(content = content, article = article, id = id)
+}
